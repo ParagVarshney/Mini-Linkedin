71a2fe5a71b9e096cb001af28f22050388227591
Parag varshney <156698917+ParagVarshney@users.noreply.github.com>
2025-08-04 13:40:42 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,13 +7,17 @@ const api = axios.create({
 // Add token to headers
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) {
+
+  // Don't send token for /auth routes (e.g., login/register)
+  if (token && !config.url.includes('/auth')) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
 export default api;
+
 
 
 
